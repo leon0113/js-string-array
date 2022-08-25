818c28954fbab5be220c3039d836d1b0ed4e3363
leon0113 <tahjib.leon399@gmail.com>
2022-08-26 02:06:36 +0600

    upper & lower case

--- case-convert.js
+++ case-convert.js
@@ -6,5 +6,9 @@ const search = 'valo';
 const userInput = 'BlackPink';
 const savedUserName = 'blackpink';
 if (userInput.toLowerCase() == savedUserName) {
-    console.log('user exits');
+    // console.log('user exits');
 }
+
+const name = 'leon';
+const upperCaseName = name.toUpperCase();
+console.log(upperCaseName);

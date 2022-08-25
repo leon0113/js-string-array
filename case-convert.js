28af67a8d8efa5aa2d000e702b8f6794f26748e0
leon0113 <tahjib.leon399@gmail.com>
2022-08-26 01:58:00 +0600

    first commit

---
+++ case-convert.js
@@ -0,0 +1,10 @@
+const anthem = 'Amr Sonar Bangla Ami Tomay Valobashi';
+const search = 'valo';
+
+
+
+const userInput = 'BlackPink';
+const savedUserName = 'blackpink';
+if (userInput.toLowerCase() == savedUserName) {
+    console.log('user exits');
+}

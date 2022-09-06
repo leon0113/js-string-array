2b491b3efb2351384797ed4b746fa19f24b4d0c8
leon0113 <tahjib.leon399@gmail.com>
2022-09-07 02:35:43 +0600

    slice

--- string-slice.js
+++ string-slice.js
@@ -1,3 +1,6 @@
 const song = 'ami tmr didhay bachi tmr didhay pore jai';
 const words = song.split(' ');
-console.log(words);
+// console.log(words);
+//slice
+const part = song.slice(3, 6);
+console.log(part);

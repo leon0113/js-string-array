3e9a49805b8b7289354e3febbbf62b022c702e79
leon0113 <tahjib.leon399@gmail.com>
2022-09-08 23:54:54 +0600

    slice & splice

---
+++ array-slice.js
@@ -0,0 +1,8 @@
+const numbers = [3, 5, 6, 9, 52, 54, 12, 985, 962, 50];
+//slice
+const sliced = numbers.slice(3, 6);
+// console.log(sliced);
+//spliced 
+const numSpliced = numbers.splice(4, 10);
+console.log(numSpliced);
+console.log(numbers);

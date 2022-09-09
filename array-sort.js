9d105adb3b5cb60cc986d4a4a37e6e0397251786
leon0113 <tahjib.leon399@gmail.com>
2022-09-09 14:57:52 +0600

    array-sort

--- array-sort.js
+++ array-sort.js
@@ -2,4 +2,8 @@ const numbers = [1, 9, 2, 5, 7, 3, 8, 50, 20];
 const sorted = numbers.sort(function (a, b) {
     return a - b;
 });
-console.log(sorted);
+// console.log(sorted);
+
+// reverse
+const reversed = numbers.reverse();
+console.log(reversed);

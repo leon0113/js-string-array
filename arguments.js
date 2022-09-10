567407ef2d30acdaa5b7e8ffe0ffc45bd1e4df9d
leon0113 <tahjib.leon399@gmail.com>
2022-09-11 03:00:07 +0600

    arguments2

--- arguments.js
+++ arguments.js
@@ -9,3 +9,15 @@ function addNumbers(num1, num2) {
 }
 const sum = addNumbers(23, 13, 50, 100, 67);
 console.log(sum);
+
+
+
+function getFullName(firstName, lastName) {
+    let fullName = ' ';
+    for (const namePart of arguments) {
+        fullName = fullName + namePart + ' ';
+    }
+    return fullName;
+}
+const name = getFullName('Tahjib', 'Hossain', 'Leon');
+console.log(name);
